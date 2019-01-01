3d7bda5262ed4f60be02a83e6812ad1e9cb32ee5
lee2sman <leetusman@gmail.com>
2018-12-31 20:35:03 -0500

    update sketch.js

--- 21/sketch.js
+++ 21/sketch.js
@@ -3,9 +3,13 @@ let cols = 5;
 let rows = 5;
 let playerX = 2;
 let playerY = 2;
+let xp = 1;
+let hp = 5;
 let prevIcon = '.';
 let numOfBoards = 0;
 let gold = 0;
+let potions = 0;
+let monsters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 function setup(){
 	noCanvas();
@@ -24,16 +28,20 @@ function createBoard(){
 		grid[i] = [];
 		for (let j=0; j < cols; j++){
 			grid[i][j] = 0;
-			if (random() < 0.001){
+			if (random() < 0.01){
 				grid[i][j] = '*';
-			} else if (random() < 0.002){
-				grid[i][j] = '&';
-			} else if (random() < 0.003){
+			} else if (random() < 0.02){
 				grid[i][j] = '?';
-			} else if (random() < 0.004){
+			} else if (random() < 0.03){
 				grid[i][j] = '!';
-			} else if (random() < 0.005){
-				grid[i][j] = '%';
+			} else if (random() < 0.05){
+				//create an enemy
+				let chooseMonster = round(random(3*xp));
+				while (chooseMonster > monsters.length){
+					chooseMonster = round(random(3*xp));
+				}
+
+				grid[i][j] = monsters[chooseMonster];
 			} else {
 				grid[i][j] = '.';
 			}
@@ -55,15 +63,16 @@ function keyPressed(){
 
 	checkKeys();
 
-	drawBoard();
-	
-	//debug
-//	print("col: "+playerX+" row: "+playerY);
+        if (hp<=0){ //did you die?
+		dead();
+        } else {
+		drawBoard();
+	}
 }
 
 function checkKeys(){
 
-    //check to see if on edge of map
+    //check to make sure not on edge of map
     if (((keyCode == UP_ARROW || key == 'k') && (playerY > 0)) || ((keyCode == DOWN_ARROW || key == 'j') && (playerY < rows-1)) || ((keyCode == LEFT_ARROW || key == 'h') && (playerX > 0)) || ((keyCode == RIGHT_ARROW || key == 'l') && (playerX < cols-1))){
     
 	//first we reset the player current position to what it was previously 
@@ -93,39 +102,64 @@ function checkKeys(){
         grid[playerY][playerX] = '@';
 	    
     } else {
+	    
+	//TODO: store old board so we can move back to it later
+
 	//OTHERWISE you must be on the edge AND moving offscreen
 	    //WRAP PLAYER AROUND to be on the other side of the screen now
 	if (keyCode === UP_ARROW || key == 'k'){
 		playerY=rows-1;	
+		createBoard();
 	}
 	if (keyCode === DOWN_ARROW || key == 'j'){
 		playerY=0;
+		createBoard();
 	}
 	if (keyCode === LEFT_ARROW || key == 'h'){
 		playerX=cols-1;
+		createBoard();
 	}
 	if (keyCode === RIGHT_ARROW || key == 'l'){
 		playerX=0;
+		createBoard();
 	}
-
-
 	    
-	//TODO: store old board so we can move back to it later
-		
-	//then create new board map - TODO: or check to see if you've already been there and load map
-	createBoard();
+	//TODO: check to see if you've already been there and load map?
 
     }
+
+	//quaff a potion
+	if (key === 'q'){
+		if (potions>0){
+			hp+=round(random(xp));	
+			potions--;
+		}
+	}
 //return false; //prevent default
 }
 
 function checkCollision(){
 
+	//have you hit an enemy?
+	if (monsters.indexOf(prevIcon) >= 0) {
+
+	    if (random() > 0.5){  //you hit monster
+		if (monsters.indexOf(prevIcon)>0){
+			prevIcon = monsters[monsters.indexOf(prevIcon)-1];
+		} else {
+			monsterKilled();
+		}
+	    } else {  //otherwise monster hits you
+		hp--;
+	    }
+
+
+	}
+
 	if (prevIcon === '*'){
 		//you've hit gold
 		//add a random amount of gold	
 		gold+=int(random(20,70));
-		createP('Total Gold: '+gold);
 
         	//now that player has taken gold, let's replace * with .
         	prevIcon = '.';
@@ -134,11 +168,46 @@ function checkCollision(){
 	if (prevIcon == '%'){
 		createP('castle');
 	}
+
+	if (prevIcon == '!'){
+		potions++;
+		prevIcon = '.'; //it's picked up so remove it
+	}
+}
+
+function monsterKilled(){
+			//remove monster from board 
+			prevIcon = '.';
+
+			//increase xp and hp
+			xp++;
+			hp+=round(random(xp));
+
 }
 
 function drawBoard(){
 	for (let i = 0; i < rows; i++){
 		createP(grid[i].join(''));
 	}
+	createP('HP '+hp);
+	createP('XP '+xp);
+	createP('Ptn '+potions);
+
+	//display total gold
+	if (gold>99){
+		createP('G:'+gold);
+	} else if (gold>9){
+		createP('Gld'+gold);
+	} else {
+		createP('Gold'+gold);
+	}
 }
 
+function dead(){
+	removeElements(); //clean out all P added to page
+createP('.....');
+createP('.---.');
+createP('|RIP|');
+createP('|...|');
+createP('|...|');
+}
