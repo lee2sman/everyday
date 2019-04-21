5c2d7d738f46143dccba4f6a72193418f873b936
lee2sman <leetusman@gmail.com>
2019-04-21 02:24:30 -0400

    update 39 sketch - sequenced loading w/ callbacks w/o preload

--- 39/sketch.js
+++ 39/sketch.js
@@ -1,4 +1,4 @@
-let soundtrack, amplitude, myFont, lourded = false, lourdX = 50;
+let soundtrack, amplitude, myFont, lourded = false, lourdX = 50, stuffImg = [], stuff=[];
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -23,7 +23,24 @@ function setup() {
 
   }));
 
-  noCursor();
+//  noCursor();
+	
+  for (let i = 0; i < 5; i++){
+    stuffImg[i] = loadImage('assets/'+i+'.png',(function(){
+    
+      if(stuffImg[4]){
+        console.log('all loaded');
+        
+	for (let i = 0; i < 5; i++){
+          stuff[i] = new Stuff();
+	}
+
+      } else {console.log('not yet loaded')}
+    })
+    
+    
+    );
+  }
 
   amplitude = new p5.Amplitude();
 
@@ -37,7 +54,16 @@ function draw() {
   } else {
     background(255);
     fill(0);
-    text('LOURDED',0,0);
+    text('TWENTY-XX',0,0);
+
+
+
+  for (let i = 0; i<5; i++){
+    stuff[i].display();
+    stuff[i].changeImage();
+    stuff[i].wander();
+  }
+	  
   }
 }
 
@@ -47,3 +73,40 @@ function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
 }
 
+class Stuff{
+  constructor(){
+    this.x = random(-width/2,width/2);
+    this.y = random(-height/2,height/2);
+    this.whichImg = floor(random(0,stuffImg.length));
+    this.level;
+    this.size;
+  }
+
+  display(){
+    push();
+
+    translate(this.x,this.y);
+
+    texture(stuffImg[this.whichImg]);
+
+    this.level = amplitude.getLevel();
+
+    this.size = map(this.level, 0, 1, 0, 2000);
+
+    ellipsoid(this.size,this.size,this.size);
+    pop();
+    }
+
+    changeImage(){
+       if (this.level>0.2){
+         this.whichImg = floor(random(0,stuffImg.length));
+       }
+    }
+    wander(){
+       if(this.level>0.2){
+         this.x+=random(-0.5,0.5);
+         this.y+=random(-0.5,0.5);
+       }
+    }
+
+}
