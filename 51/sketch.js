0a51d1ee8dea27d436f307b5b8d4c91654ee88d8
lee2sman <leetusman@gmail.com>
2019-05-08 03:57:10 -0400

    add 51

---
+++ 51/sketch.js
@@ -0,0 +1,83 @@
+let loaded = false;
+let mark, totalObjects, teapot, pots = [], skins=[];
+let song;
+let timer = 5000;
+let rubik;
+
+function preload(){
+  for (let i = 0; i < 7; i++){
+    skins[i] = loadImage('assets/dazzle'+i+'.jpg');
+  }
+	let rubik = loadImage('https://media.giphy.com/media/12MdYw1jNtzqUM/giphy.gif');
+//  mark = loadModel('assets/Paul.stl.obj', true);
+  song = loadSound('assets/eno.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+
+  totalObjects = 8;
+
+  for (let i = 0; i<totalObjects; i++){
+    pots[i] = new Teapot();
+  }
+ 
+	ambientLight(150);
+  ambientMaterial(250);
+
+}
+
+function draw(){
+	if (millis()>timer){
+	  song.play();
+          timer+=random(4000,8000);
+	}
+	rotateX(20);
+
+	for (let i = 0; i < totalObjects ; i++){
+          pots[i].display();
+          pots[i].changeDir();
+	}
+
+  if (random()<0.02){
+    clear();
+    let b = select('body');
+    b.style("background","linear-gradient(to bottom, rgba("+random(255)+","+random(255)+","+random(255)+","+random(255)+") "+random(100)+"%, rgba("+random(255)+","+random(255)+","+random(255)+","+random(255)+") "+random(100)+"%)").style("background-size"," 80% 75%");
+  }
+
+}
+
+
+class Teapot{
+  constructor(){
+    this.x = random(-width/2,width/2,);
+    this.y = random(-height/2,height/2);
+    this.potScale = random(0.5,2);
+    this.xRot = random(0.05);
+    this.yRot = random(0.05);
+    this.timer = 3000;
+    this.skin = skins[round(random(totalObjects - 1))];
+  }
+
+  display(){
+    push();  
+    rotateX(this.xRot);
+    rotateY(this.yRot);
+    translate(this.x, this.y);
+
+    texture(this.skin);
+    //model(mark);
+    plane(900);
+    pop();
+  }
+  
+  changeDir(){
+   if (millis()>this.timer){
+      this.xRot = random(0.05);
+      this.yRot = random(0.05);
+      this.timer+=random(500,1000);
+    }
+  }
+}
+
+
