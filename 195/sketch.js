6e0b4b6dab88626d9ee2b9b8a06c33ca22ff2a45
lee2sman <leetusman@gmail.com>
2021-10-07 04:35:17 -0400

    bump

---
+++ 195/sketch.js
@@ -0,0 +1,28 @@
+let merzbau = []
+let totalImg = 12
+function preload(){
+  for (let i = 0; i < totalImg; i++){
+    merzbau[i] = loadImage('assets/'+i+'.png')
+  }
+}
+function setup(){
+  //createCanvas(windowWidth,windowHeight)
+		createCanvas(4921, 7360); //resize for 300dpi!
+  background(0)
+}
+function draw(){
+  for (let i = 0; i < 5; i++){
+    image(random(merzbau),random(-width/2,width/2),random(-height/2,height/2),random(width/8,width*2),random(width/8,width*2))
+  }
+  filter(THRESHOLD)
+  noLoop()
+}
+function mousePressed(){
+  redraw()
+}
+function keyPressed(){
+  if (key == 's'){
+    save()
+  }
+}
+
