0d192c9e2cba0651f436019e88f5985c886120e6
lee2sman <leetusman@gmail.com>
2021-10-12 02:40:58 -0400

    update 198

--- 198/sketch.js
+++ 198/sketch.js
@@ -10,17 +10,18 @@ function preload() {
 function setup() {
 	//createCanvas(400,400)
 	createCanvas(4921, 7360); //resize for 300dpi!
-	step = width / 6;
+	step = width / 12;
 
-  background(0)
+  //background(255)
 	for (let y = 0; y < height; y += step) {
 		for (let x = 0; x < width; x += step) {
 
+			image(random(merzbau), x, y, step, step);
 		  if (random()>0.5){fill(0);} else {fill(255)}
+		  if (random()>0.5){rotate(random(radians(360)))}
 		//  rect(x,y,step,step)
-			//image(random(merzbau), x, y, step, step);
-		  let _scale = ceil(random(0.1,1.5))
-image(merzbau[7], x, y, step*_scale, step*_scale, random(step), random(step),step,step)
+		  let _scale = ceil(random(0.5,1.5))
+image(random(merzbau), x, y, step*_scale, step*_scale, random(step), random(step),step,step)
 
 		}
 	}
