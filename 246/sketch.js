694969058b754b7cb566eb0c6c88131d3f195e9a
lee2sman <leetusman@gmail.com>
2022-08-02 20:54:15 +0200

    update: twice as big?

--- 246/sketch.js
+++ 246/sketch.js
@@ -1,5 +1,4 @@
-const adjs = ["Action","Alex","Andre","Apathy","Arms","Averick","Bad","Bean","Big","Bizzy","Black","Bobby","Boosie","Brotha","Brother","Bun","Bushwick","Busta","Camron","Canibus","Chief","Chip","Chris","Chubby","Chuck","Cole","Common","Coolio","Craig","Crooked","Darth","Dirty","Dopey","Dr.","Earl","Elbow","Fabulous","Fat","Fence","Fifteen","Freddie","Garage","Ghostface","Giant","Good","Great","Green","Guns","Guru","Height","Hoppy","Ice","Immortal","Inspectah","Iron","Itchy","Jaded","Jay","Joey","Jump","Kendrick","Kid","Killer","King","Knife","Kool","Krayzie","Lanky","Lauryn","Layzie","Legs","Love","Lovely","Lupe","Mac","Masta","MC","Mean","Medium","Meek","Meet","Method","Moons","Mos","Nas","Nate","Notorious","Paul","Pimp","Pisco","Presence","Prince","Prodigy","Pusha","Quantity","Ravine","Regal","Rick","Rittz","Rotten","Royce","Saint","Sault","Scarface","Sedge","Sheen","Sick","Sixteen","Skinny","Slick","Slug","Smelly","Smuth","Snoop","Sour","Spleen","Structure","Suns","Talib","Tall","Tech","Toadally","Toaday","Tons","Tread","Twisted","Tyler","Usual","Utter","Vinnie","Wally","Wealth","Wiz"]
-let cube = 100;
+let cube = 400;
 let result;
 
 function preload() {
@@ -10,11 +9,11 @@ function setup(){
   createCanvas(cube,cube);
   noStroke()
   background(0);
-  frameRate(1)
+  frameRate(0.25)
+  textSize(cube/4);
 }
 
 function draw(){
-
   fill(random(255),random(255),random(255))
 
   if(random()<0.07){
@@ -22,8 +21,8 @@ function draw(){
   }
   rect(0,0,cube,cube);
 
-    for (let x = 0; x < width; x+=50){
-      for (let y = 0; y < height; y+=50){
+    for (let x = 0; x < width; x+=cube/2){
+      for (let y = 0; y < height; y+=cube/2){
 
 	if (random()<0.15){
 
@@ -37,19 +36,20 @@ function draw(){
 	}
 
 	  //random add letter
-	    if (random()<0.05){
+	    if (random()<0.5){
 	      fill(random(255),random(255),random(255))
 	      let abc = 'abcdefghijklmnopqrstuvwxyz'
-	      text(abc[ceil(random()*abc.length)],x+cube/4,y-cube/4);
+	      text(abc[ceil(random()*abc.length)],x+cube,y-cube);
 	    }
 
 	  //random add circle
 	    if (random()<0.12){
 	        fill(random(255),random(255),random(255));
-                ellipse(x+25,y+25,50);
+                ellipse(x+cube/4,y+cube/4,cube/2);
 	    }
 
       }
+
     }
 
   removeElements()
