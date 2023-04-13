9fb9a2e5ac25927393aac24e2757c992c5fe9dcc
lee2sman <leetusman@gmail.com>
2023-04-13 15:40:10 -0400

    update 278 with name generator. make face fill canvas

--- 278/sketch.js
+++ 278/sketch.js
@@ -1,37 +1,61 @@
-let maxHair = 8
-let maxSetOfEyes = 4
-let maxFace = 3
-let eyes = []
-let hair = []
-let face = []
-let whichFace, whichHair, whichEyes
+let maxHair = 8;
+let maxSetOfEyes = 4;
+let maxFace = 3;
+let eyes = [];
+let hair = [];
+let face = [];
+let whichFace, whichHair, whichEyes;
 
-function preload(){
-
-  for (let i=0;i<=maxSetOfEyes;i++){
-    eyes[i] = loadImage("eyes" + i + ".png")
-  }
-  for (let i=0;i<=maxHair;i++){
-    hair[i] = loadImage("hair"+i+".png");
-  }
-  for (let i=0;i<=maxFace;i++){
-    face[i] = loadImage("face"+i+".png");
-  }
-
+function preload() {
+	for (let i = 0; i <= maxSetOfEyes; i++) {
+		eyes[i] = loadImage("eyes" + i + ".png");
+	}
+	for (let i = 0; i <= maxHair; i++) {
+		hair[i] = loadImage("hair" + i + ".png");
+	}
+	for (let i = 0; i <= maxFace; i++) {
+		face[i] = loadImage("face" + i + ".png");
+	}
 }
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
 
-
-  whichFace = random(face)
-   whichHair = random(hair)
-   whichEyes = random(eyes)
+	imageMode(CENTER);
+	chooseFeatures();
 }
 
 function draw() {
-  background(220);
+	background(248);
 
-  image(whichFace,0,0,60,120)
-  image(whichHair,0,0,60,40)
-  image(whichEyes,3,40)
+	image(whichFace, width / 2, height / 2, width / 2, height);
+	image(whichHair, width / 2, height / 8, width / 2, height / 3);
+	image(whichEyes, width / 2, height / 2, width / 2, height / 6);
+	fill(0);
+	text(npcName, 20, 20);
+}
+
+function chooseFeatures() {
+	whichFace = random(face);
+	whichHair = random(hair);
+	whichEyes = random(eyes);
+	npcName = randomName();
+}
+
+function mousePressed() {
+	chooseFeatures();
+}
+
+function keyPressed() {
+	chooseFeatures();
+}
+function randomName() {
+	let characters = "abcdefghijklmnopqrstuvwxyz";
+	let nameStr = characters
+		.charAt(Math.floor(Math.random() * characters.length))
+		.toUpperCase();
+	let nameLength = floor(random(4, 8));
+	for (let i = 0; i < nameLength; i++) {
+		nameStr += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return nameStr;
 }
