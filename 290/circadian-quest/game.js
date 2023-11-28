91912641309127cf1ac31bbad3f9dc04cb55cad0
lee2sman <leetusman@gmail.com>
2023-11-28 02:14:27 -0500

    update v1.1

--- 290/circadian-quest/game.js
+++ 290/circadian-quest/game.js
@@ -4,6 +4,22 @@ let hero = document.createElement("img");
 hero.setAttribute("src", "../assets/hero.gif");
 document.body.prepend(hero);
 
+//initialize cal
+const d = new Date();
+const year = d.getFullYear();
+const month = d.getMonth();
+
+const date = new Date(year, month, 1);
+const firstDay = date.getDay()
+
+//add blank divs
+for (let day=1;day<=firstDay;day++){
+  const newDay = document.createElement("div");
+  newDay.style.backgroundColor = "darkseagreen";
+  newDay.innerHTML = "<div></div>";
+  document.getElementsByTagName("main")[0].appendChild(newDay);
+}
+
 //initialize map/days
 //set as trash
 for (let day=1;day<=30;day++){
@@ -13,7 +29,8 @@ for (let day=1;day<=30;day++){
 }
 
 //add successful day imgs
-let successfulDay = document.querySelector('main :nth-child(16)');
+let fixDay = 16 + firstDay;
+let successfulDay = document.querySelector("main :nth-child("+fixDay+")");
 successfulDay.style.backgroundColor = "green";
 
 let dayImg = successfulDay.getElementsByTagName("img")[0];
@@ -22,5 +39,5 @@ xp++;
 
 //add text logs
 const daylog = document.createElement("p");
-daylog.innerText = "16: Walked around neighborhood a long while enjoying warm weather and seeing leaves changed color. Enjoyed this japanese maple.";
+daylog.innerText = "16: Took a long walk around Ditmas Park this afternoon as I practice building my lung capacity post-covid, enjoying the warm weather and seeing gorgeous leaves of many colors. My favorite was this japanese maple.";
 document.body.appendChild(daylog);
